713bc9f806c057ec1d3085bca00d972ca999bc13
AZAT DİCLE <75863129+azatdicle@users.noreply.github.com>
2024-03-28 23:54:16 +0300

    Create network_scan.py

---
+++ network_scan.py
@@ -0,0 +1,29 @@
+import scapy.all as scapy 
+import optparse
+print(""" 
+┏┓┏┓┏┓┏┳┓  ┳┓┳┏┓┓ ┏┓             
+┣┫┏┛┣┫ ┃   ┃┃┃┃ ┃ ┣              
+┛┗┗┛┛┗ ┻   ┻┛┻┗┛┗┛┗┛             
+┳┓┏┓┏┳┓┓ ┏┏┓┳┓┓┏┓  ┏┓┏┓┏┓┳┓┳┓┏┓┳┓
+┃┃┣  ┃ ┃┃┃┃┃┣┫┃┫   ┗┓┃ ┣┫┃┃┃┃┣ ┣┫
+┛┗┗┛ ┻ ┗┻┛┗┛┛┗┛┗┛  ┗┛┗┛┛┗┛┗┛┗┗┛┛┗
+         @azatmurg
+      Network Scanner      
+      """)
+def get_user_input():
+    parse_input=optparse.OptionParser()
+    parse_input.add_option("-i","--ipaddress",dest="ip_address",help="Enter ip Address")
+    (user_input,argument)=parse_input.parse_args()
+    if not user_input.ip_address:
+        print("Please Enter the ip address")
+    return user_input
+        
+def scan_my_network(ip):
+    arp_request_packet=scapy.ARP(psrc=ip)
+    brodcast_packet=scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    combinebacket=arp_request_packet/brodcast_packet
+    (answered_list,unanswered_list) = scapy.srp(combinebacket,timeout=1)
+    answered_list.summary()
+    
+user_ip_address = get_user_input()
+scan_my_network=(user_ip_address.ip_address)
